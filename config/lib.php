03b1ee2b9dbd5e4fdd8b90b6c6dac1bee9aec1b4
arti-max <128210100+arti-max@users.noreply.github.com>
2023-11-27 12:20:24 +0300

    Add files via upload

---
+++ config/lib.php
@@ -0,0 +1,25 @@
+<?php
+include_once __DIR__ . "/ip_in_range.php";
+class Lib {
+public function getUserID($extID, $userName = "Undefined") {
+		include __DIR__ . "/connection.php";
+		if(is_numeric($extID)){
+			$register = 1;
+		}else{
+			$register = 0;
+		}
+		$query = $db->prepare("SELECT userID FROM users WHERE extID LIKE BINARY :id");
+		$query->execute([':id' => $extID]);
+		if ($query->rowCount() > 0) {
+			$userID = $query->fetchColumn();
+		} else {
+			$query = $db->prepare("INSERT INTO users (isRegistered, extID, userName, lastPlayed)
+			VALUES (:register, :id, :userName, :uploadDate)");
+
+			$query->execute([':id' => $extID, ':register' => $register, ':userName' => $userName, ':uploadDate' => time()]);
+			$userID = $db->lastInsertId();
+		}
+		return $userID;
+	}
+}
+    ?>
