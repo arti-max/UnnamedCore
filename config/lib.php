24cb28a3137c38f9278b89b9eda6282f46e8c20e
arti-max <128210100+arti-max@users.noreply.github.com>
2023-11-27 13:15:50 +0300

    Add files via upload

--- config/lib.php
+++ config/lib.php
@@ -1,25 +1,42 @@
 <?php
 include_once __DIR__ . "/ip_in_range.php";
-class Lib {
-public function getUserID($extID, $userName = "Undefined") {
+class lib {
+	public function getUserID($udid, $userName = "Undefined") {
 		include __DIR__ . "/connection.php";
-		if(is_numeric($extID)){
-			$register = 1;
-		}else{
-			$register = 0;
-		}
-		$query = $db->prepare("SELECT userID FROM users WHERE extID LIKE BINARY :id");
-		$query->execute([':id' => $extID]);
+		$query = $db->prepare("SELECT userID FROM users WHERE udid LIKE BINARY :id");
+		$query->execute([':id' => $udid]);
 		if ($query->rowCount() > 0) {
 			$userID = $query->fetchColumn();
 		} else {
-			$query = $db->prepare("INSERT INTO users (isRegistered, extID, userName, lastPlayed)
-			VALUES (:register, :id, :userName, :uploadDate)");
+			$query = $db->prepare("INSERT INTO users (udid, userName)
+			VALUES (:id, :userName)");
 
-			$query->execute([':id' => $extID, ':register' => $register, ':userName' => $userName, ':uploadDate' => time()]);
+			$query->execute([':id' => $udid, ':userName' => $userName]);
 			$userID = $db->lastInsertId();
 		}
 		return $userID;
 	}
+	public function getUDID($userID) {
+		include __DIR__ . "/connection.php";
+		$query = $db->prepare("SELECT udid FROM users WHERE userID = :id");
+		$query->execute([':id' => $userID]);
+		if ($query->rowCount() > 0) {
+			return $query->fetchColumn();
+		}else{
+			return 0;
+		}
+	}
+	public function getAudioTrack($id) {
+		$songs = ["Stereo Madness by ForeverBound",
+			"Back on Track by DJVI",
+			"Polargeist by Step",
+			"Dry Out by DJVI",
+			"Base after Base by DJVI",
+			"Can't Let Go by DJVI",
+			"Jumper by Waterflame"];
+		if($id < 0 || $id >= count($songs))
+			return "Unknown by DJVI";
+		return $songs[$id];
+	}
 }
-    ?>
+?>
